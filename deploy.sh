62c934340b88432b675de37db5f74db6fca7a62a
copernicus-mogley <ryan@9wizards.com>
2014-02-12 15:04:37 -0800

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+PACKAGE=.
+REMOTEDIR=/var/www/www_ethereum_org/public_html/
+KEYPATH=~/Downloads/prod-web.pem
+SERVERS=(54.213.131.123 54.213.131.108)
+
+for ix in ${!SERVERS[*]}
+do
+    printf "......................\nConnecting to %s...\n" "${SERVERS[$ix]}"
+    rsync -avzl -e "ssh -i ${KEYPATH}" --delete --exclude ".git" ${PACKAGE} ubuntu@${SERVERS[$ix]}:${REMOTEDIR}
+done
