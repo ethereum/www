e8e17bfd3050877c026b2160f2499a5549b6752a
Taylor Gerring <taylor.gerring@gmail.com>
2014-06-26 21:45:32 +0200

    enclose scrollUp in anonymous function

--- src/scripts/libs/main.js
+++ src/scripts/libs/main.js
@@ -17,21 +17,24 @@ $(function() {
     }, 400, 'swing');
   });
 
-  $.scrollUp({
-    scrollName: 'scrollUp', // Element ID
-    scrollDistance: 300, // Distance from top/bottom before showing element (px)
-    scrollFrom: 'top', // 'top' or 'bottom'
-    scrollSpeed: 300, // Speed back to top (ms)
-    easingType: 'linear', // Scroll to top easing (see http://easings.net/)
-    animation: 'fade', // Fade, slide, none
-    animationInSpeed: 200, // Animation in speed (ms)
-    animationOutSpeed: 200, // Animation out speed (ms)
-    scrollText: '<i class="fa fa-fw fa-caret-up fa-2x">', // Text for element, can contain HTML
-    scrollTitle: false, // Set a custom <a> title if required. Defaults to scrollText
-    scrollImg: false, // Set true to use image
-    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-    zIndex: 2147483647 // Z-Index for the overlay
+  $(function() {
+    $.scrollUp({
+      scrollName: 'scrollUp', // Element ID
+      scrollDistance: 300, // Distance from top/bottom before showing element (px)
+      scrollFrom: 'top', // 'top' or 'bottom'
+      scrollSpeed: 300, // Speed back to top (ms)
+      easingType: 'linear', // Scroll to top easing (see http://easings.net/)
+      animation: 'fade', // Fade, slide, none
+      animationInSpeed: 200, // Animation in speed (ms)
+      animationOutSpeed: 200, // Animation out speed (ms)
+      scrollText: '<i class="fa fa-fw fa-caret-up fa-2x">', // Text for element, can contain HTML
+      scrollTitle: false, // Set a custom <a> title if required. Defaults to scrollText
+      scrollImg: false, // Set true to use image
+      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+      zIndex: 2147483647 // Z-Index for the overlay
+    });
   });
+
 
   $('#countdown').countdown({
     until: $.countdown.UTCDate(0, new Date(2014, 03 - 1, 01)),
