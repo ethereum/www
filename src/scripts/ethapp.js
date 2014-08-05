412856027c7afb3dc3407a4a161d8f3423ca3b3f
Marian Oancea <contact@siteshop.ro>
2014-08-06 01:52:56 +0300

    Prep for staging

--- src/scripts/ethapp.js
+++ src/scripts/ethapp.js
@@ -1,4 +1,5 @@
-ETHERSALE_URL = "https://sale.ethereum.org";
+// ETHERSALE_URL = "https://sale.ethereum.org";
+ETHERSALE_URL = "https://devsale.ethereum.org";
 SELF_URL = "https://www.ethereum.org/";
 BLOCKCHAIN_URL = "https://blockchain.info";
 BLOCKCHAIN_API = "5b846ae8-eb56-4c14-aae9-bd13056b6df7";
