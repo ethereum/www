d8bbdaf9bc23059c7b73f203ebe2468fd8756d24
Marian Oancea <contact@siteshop.ro>
2014-08-05 21:20:15 +0300

    Added staging sale hostname for testing

--- src/scripts/ethapp.js
+++ src/scripts/ethapp.js
@@ -1,4 +1,5 @@
-ETHERSALE_URL = "https://sale.ethereum.org";
+// ETHERSALE_URL = "https://sale.ethereum.org";
+ETHERSALE_URL = "https://ksd8u2ijnrefu287ygh2.elasticbeanstalk.com";
 SELF_URL = "https://www.ethereum.org/";
 BLOCKCHAIN_URL = "https://blockchain.info";
 BLOCKCHAIN_API = "5b846ae8-eb56-4c14-aae9-bd13056b6df7";
